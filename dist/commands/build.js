2623c1c663e33ac01ebd3885fdfeacacd399612d
Lucas Meira <lucasmeira007@hotmail.com>
2024-12-23 19:20:17 -0300

    Delete dist directory

--- dist/commands/build.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.buildProject = void 0;
-const run_1 = require("./run");
-const buildProject = async () => {
-    return (0, run_1.runCommand)('npm run build');
-};
-exports.buildProject = buildProject;
